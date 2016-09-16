e2c984151fd5f2893a9db75611a3de1574c861e3
Coder206 <patrick.arrow206@outlook.com>
2016-09-15 20:20:17 -0400

    Ajouts SW, jour et nuit

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ function w3_open() {
 function w3_close() {
     document.getElementsByClassName("w3-sidenav")[0].style.display = "none";
     document.getElementsByClassName("w3-overlay")[0].style.display = "none";
-
 }
 
 window.onscroll = function () {
