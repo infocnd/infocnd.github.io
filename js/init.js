03c6858fe7d1938ed45981a4934fd7b836d326cc
Coder206 <Coder206@users.noreply.github.com>
2016-09-20 21:15:50 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,7 +1,25 @@
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/js/serviceWorker.js').then(function(reg) {
     console.log('Yey!', reg);
   }).catch(function(err) {
     console.log('Boo!', err);
   });
 }
+*/
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/js/serviceWorker.js', { scope: '/sw-test/' }).then(function(reg) {
+    
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+    
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+};
