b926c1bab71504d779454e0eb712dc709fb4fd03
Patrick <patrick.arrow206@outlook.com>
2016-10-20 10:46:06 +0000

    Fermer le menu

--- js/nav.js
+++ js/nav.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
             $("#cnd-contenu").attr("data-include", $(this).attr("data-link"));
             render();
             w3_close();
-            divParentCours = String($(this).parents('div:first').attr('id'));
+            divParentCours = String($(this).parents('div:eq(0)').attr('id'));
             divParentAnnee = String($(this).parents('div:eq(1)').attr('id'));
             myFunc(divParentCours);
             myFunc(divParentAnnee);
